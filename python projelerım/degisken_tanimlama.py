dc7b23d93dfeb4ab7307666d1b20fda282eb4978
eren_akcnn <akcaneren18@gmail.com>
2026-04-20 18:31:46 +0300

    degişkenler ve input calısıldı

---
+++ python projelerım/degisken_tanimlama.py
@@ -0,0 +1,20 @@
+# eiımizde 2000 liralik bi urun var ve bu urunune %18ini eklemek istiyoruz
+
+urunAFıyat = 2000 #bu demek oluyo kı urunAFıyat yazan her yere 2000 yaz
+urunBFıyat = 3000 # bu da demek oluyo kı urunBFıyat yazan her yere 3000 yaz
+kdvOrani = 0.20
+
+print(urunAFıyat + (urunAFıyat * kdvOrani)) 
+print(urunBFıyat + (urunBFıyat * kdvOrani))
+
+#mesela bu kdv oranını degıstırmek ıstedıgımde tek tek butun satırları degıstırmek yerıne;
+#kdv oranına da bı degısken verırım ve sayı yerıne bu kdv oranını yazarım 
+#ve verdıgım degıskenın sayı degerını degıstırmem yeterlı olacaktır
+# urun fıyaltlarını toplamak ıstersek;
+print(urunAFıyat + urunBFıyat)
+
+
+
+
+
+# eger tırnak ısaretı kullansaydık toplama carpma ıslemlerını yapmaz dırekt yazanları yazardı
---
+++ python projelerım/degıskenler.py
@@ -0,0 +1,5 @@
+isim = input("adın ne kanka?") # gelen ısmı ısım degıskenıne kaydet demek
+print("memnun oldum" + isim)
+print(isim)
+# input sorunun cevabını alıp verılen degıskene atar 
+# mesela adın ne kanka dedık ve cevabı yazdık o cevap artık ısım degıskenını temsıl eder 
