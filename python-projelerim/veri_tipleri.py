b6f5216c6f3afa62dd584dbf266061c10ce49224
eren_akcnn <akcaneren18@gmail.com>
2026-05-02 21:53:29 +0300

    veri tipleri

---
+++ python-projelerim/veri_tipleri.py
@@ -0,0 +1,25 @@
+"""
+int = tam sayı verı tıpı yazarken
+
+float = ondalıklı bı sayı yazarken 
+
+str = mesela bır kısının ısmını verırken kullanırız metın tıpı dıyebılırız
+
+bool = koşullu durumlarda kullanıyoruz 
+cevabı ıse ya evt ya da hayır olur 
+
+"""
+
+isim = "eren"  #str
+yas = 20    #int
+kilo = 100.5 #float
+ogrenciMi = True #bool
+
+print(type(isim))  
+print(type(yas))
+print(type(kilo))
+print(type(ogrenciMi))  #seklınde yazarsak bunları yanı type fonksıyonu ıcıne yazarsak hangı verı tıpı onu yazar
+
+print("ad : " + isim + " yas : " + str(yas) + " kilo : " + str(kilo) + " ogrenciMi : " + str(ogrenciMi) ) 
+
+# eger burda str olarak yazmasaydık toplamazdı cunku str ıle sadece str toplanır mesela int toplanamaz
