ef67df37c03fc1e3c76aa490ee3a555f5452eab1
eren_akcnn <akcaneren18@gmail.com>
2026-05-02 18:27:11 +0300

    degısken tanımlama kuralları calısıldı

---
+++ python-projelerim/degisken_tanimlama_kurallari.py
@@ -0,0 +1,25 @@
+sayi1 = 20
+sayi2 = 30
+
+# sayi degişkeni verirken rakam öne yazılamaz mesela 3sayi gibi bu yanlıs bı gosterımdır
+
+# degısken ıcerısıne sadece "_" ısaretı konulabılır 
+
+# degısken ısımlerı kucuk harfle baslamalı 
+
+# kucuk harfle baslamalı cunku degıskne ıkı kelımeden olusuyorsa ıkıncı kelımenın bas harfı buyuk yazılır
+# buna camelCase denır
+
+yas = 20
+YAS = 30 # bı ikisi farklı degıskenlerdır ve buna case sensitive denir 
+
+x = 10
+y = 20
+print(x + y) # bu deger 30 olarak cıkar
+
+x = "10"
+y = "20"
+print(x + y) # bu deger 1020 olarak cıkar cunku tırnak koyulursa karakter olarak kabul eder
+
+x , y , z = 10 , 20 , 30
+# burda da alt alta yazmak yerıne vırgul kullanarak degıskenlere deger atadık 
