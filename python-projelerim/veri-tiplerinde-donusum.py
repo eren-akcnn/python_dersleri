bc3918593ff3cc6567d7d73613d13c204938302d
eren_akcnn <akcaneren18@gmail.com>
2026-05-03 19:17:03 +0300

    veri-tiplerinde donusum calısıldı

---
+++ python-projelerim/veri-tiplerinde-donusum.py
@@ -0,0 +1,23 @@
+# sayi1 = int(input("sayi 1 :"))
+
+# sayi2 = int(input("sayi 2 :"))
+
+# toplam = sayi1 + sayi2
+# print(toplam)
+
+# mesela ;
+
+x = int("1")
+
+print(x)  # yanı burda strıng ıfadesı yazmamıza ragmen tırnak ısaretı kullandık cunku in verı tıpını 
+          #   kullandıgımız ıcın sayı olan 1 i yazdı 
+
+# x = int(10.5)  burda int olarak gonderdıgımz ıcın ondalıklı sayının sadece tam kısmını yazdı 
+
+print(x)
+
+# ama int olarak degıdle str olarak yazsaydık yanı int("10.5") hata verırdı 
+
+x = float("10.2") 
+
+print(x)
